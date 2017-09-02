8aa737ee744368e692d46f2e156701796c5ab29c
yadira02 <30930945+yadira02@users.noreply.github.com>
2017-09-02 03:28:50 +0000

    Add files via upload
    burbuja

---
+++ programa de burbuja.py
@@ -0,0 +1,15 @@
+#declaracion de funciones
+def burbuja(A):
+	for i in range(1,len(A)):
+		for j in range(0,len(A)-1):
+			if(A[j+1]<A[j]):
+				aux=A[j]
+				A[j]=A[j+1]
+				A[j+1]=aux
+	print(A)
+
+#programa principal
+A=[6,5,3,1,8,7,2,4]
+print(A)
+burbuja(A)
+input("precione enter para continuar")

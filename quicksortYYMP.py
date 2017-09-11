6bbd875526d41ff07f5664125f036e2281aaf106
yadira02 <30930945+yadira02@users.noreply.github.com>
2017-09-11 16:49:13 +0200

    Add files via upload
    programa correcto.

--- quicksortYYMP.py
+++ quicksortYYMP.py
@@ -3,30 +3,34 @@ cnt = 0
 
 def quicksort(arr):
 	global cnt
-	if len(arr) <= l:
+	if len(arr) < 2:
 		return arr
 	p = arr.pop(0)
-	menores, mayores = [], []
+	menores, mayores = [],[]
 	for e in arr:
-		cnt+=1
+		cnt +=1
 		if e <= p:
 			menores.append(e)
 		else:
 			mayores.append(e)
-	return quicksort(menores) +[p] + quicksort(mayores)
+	return quicksort(menores) + [p] + quicksort(mayores) 
 
 def rndar(long):
 	arr = []
 	for i in range(long):
-		arr.append(random,randint(0,long))
+		arr.append(random.randint(0,long))
 	return arr
-l=10
+
+
+
+l = 10
+
 
 
 while l <= 10:
 	for replica in range(10):
 		ori = rndar(l)
-		arr = quicksort(ori)
-		print(l, cnt, arr, ori)
+		arr = quicksort(ori)	
+		print( l, cnt, arr, ori)
 		cnt = 0
 	l*=2
